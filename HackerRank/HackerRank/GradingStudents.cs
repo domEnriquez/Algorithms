4e8e81bc2850f073d7ed7fd9c4a5e54b50806ca6
Dominic <dominicjosephenriquez@gmail.com>
2018-02-14 21:17:59 +0900

    GradingStudents Challenge

---
+++ HackerRank/HackerRank/GradingStudents.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    [TestFixture]
+    public class GradingStudents
+    {
+        private const int BOUNDARY_GRADE = 38;
+
+        private void assertRoundUp(int Actual, int Expected)
+        {
+            Assert.AreEqual(Expected, round(Actual));
+        }
+
+        [Test]
+        public void GivenGrades_WhenTriedToRoundUp_ThenRoundUpAccordingToRules()
+        {
+            assertRoundUp(0, 0);
+            assertRoundUp(1, 1);
+            assertRoundUp(35, 35);
+            assertRoundUp(BOUNDARY_GRADE, 40);
+            assertRoundUp(39, 40);
+            assertRoundUp(41, 41);
+            assertRoundUp(43, 45);
+            assertRoundUp(73, 75);
+            assertRoundUp(67, 67);
+        }
+
+
+
+        private int round(int num)
+        {
+            if (num >= BOUNDARY_GRADE)
+            {
+                int remainder = num % 5;
+
+                if (remainder == 3 || remainder == 4)
+                    return num / 5 * 5 + 5;
+                else
+                    return num;
+            }
+
+            return num;
+        }
+    }
+}
