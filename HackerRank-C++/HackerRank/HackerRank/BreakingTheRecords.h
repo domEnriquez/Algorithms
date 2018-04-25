fb825e656119ee4357d6ed3831e8741da02dc617
ph000007 <ph000007@ACPH0007>
2018-04-25 12:29:06 +0800

    Solved Breaking the Records problem using C++

---
+++ HackerRank-C++/HackerRank/HackerRank/BreakingTheRecords.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+namespace HackerRank {
+	class BreakingTheRecords {
+		public:
+			int CountMaxBreakRecord(vector<int> scores);
+			int CountMinBreakRecord(vector<int> scores);
+	};
+}
