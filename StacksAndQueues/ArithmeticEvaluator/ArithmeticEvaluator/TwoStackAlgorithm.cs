58a9652ad0bcba2f09d0edf987a241bdf10899db
ph000007 <ph000007@ACPH0007>
2018-05-02 17:36:20 +0800

    Arithmetic expression evaluation using two stack algorithm

---
+++ StacksAndQueues/ArithmeticEvaluator/ArithmeticEvaluator/TwoStackAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using GenericStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticEvaluator
+{
+    public class TwoStackAlgorithm
+    {
+        public double Evaluate(string expression)
+        {
+            ResizingArrayStack<char> ops = new ResizingArrayStack<char>();
+            ResizingArrayStack<double> vals = new ResizingArrayStack<double>();
+
+            for(int i = 0; i < expression.Length; i++)
+            {
+                char currChar = expression[i];
+                if (currChar == '(') ;
+                else if (currChar == '+') ops.Push(expression[i]);
+                else if (currChar == '*') ops.Push(expression[i]);
+                else if (currChar == ')')
+                {
+                    char op = ops.Pop();
+                    if (op == '+')
+                        vals.Push(vals.Pop() + vals.Pop());
+                    else if (op == '*')
+                        vals.Push(vals.Pop() * vals.Pop());
+                }
+                else vals.Push(Char.GetNumericValue(currChar));
+            }
+
+            return vals.Pop();
+        }
+    }
+}
---
+++ StacksAndQueues/ArithmeticEvaluator/ArithmeticEvaluatorTest/ArithmeticEvaluatorTest.cs
@@ -0,0 +1,22 @@
+﻿using ArithmeticEvaluator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticEvaluatorTest
+{
+    [TestFixture]
+    public class ArithmeticEvaluatorTest
+    {
+        [Test]
+        public void AcceptanceTest()
+        {
+            TwoStackAlgorithm eval = new TwoStackAlgorithm();
+
+            Assert.AreEqual(101.0, eval.Evaluate("(1+((2+3)*(4*5)))"));
+        }
+    }
+}
