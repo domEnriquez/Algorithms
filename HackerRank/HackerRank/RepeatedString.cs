ce573069ddcbaef4868711ff8c5262293e4dd99d
Dominic <dominicjosephenriquez@gmail.com>
2021-04-02 20:58:30 +0200

    Solved Repeated String and 2D Array problem

---
+++ HackerRank/HackerRank/RepeatedString.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank
+{
+    [TestFixture]
+    public class RepeatedString
+    {
+        private void assertCountOfA(string s, long n, long expected, string comment)
+        {
+            Assert.AreEqual(expected, repeatedString(s, n), comment);
+        }
+
+        [Test]
+        public void canReturnHowManyAs()
+        {
+            assertCountOfA(null, 0, -1, "1");
+            assertCountOfA("", 0, 0, "2");
+            assertCountOfA("a", 0, 0, "3");
+            assertCountOfA("b", 1, 0, "4");
+            assertCountOfA("a", 1, 1, "5");
+            assertCountOfA("aa", 1, 1, "6");
+            assertCountOfA("aa", 2, 2, "7");
+            assertCountOfA("a", 2, 2, "8");
+            assertCountOfA("aba", 10, 7, "9");
+            assertCountOfA("a", 1000000000000, 1000000000000, "10");
+            assertCountOfA("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm",
+                736778906400,
+                51574523448,
+                "11");
+        }
+
+
+        private long repeatedString(string s, long n)
+        {
+            if (s == null)
+                return -1;
+
+            if (s == string.Empty || n == 0 || !s.Contains("a"))
+                return 0;
+
+            if (s.Length == 1)
+                return n;
+
+            int stringSize = s.Length;
+
+            return countA(s, stringSize) * (n / stringSize) 
+                + countA(s, n % stringSize);
+
+        }
+
+        private static long countA(string s, long upTo)
+        {
+            int count = 0;
+
+            for (int i = 0; i < upTo; i++)
+            {
+                if (s[i] == 'a')
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ HackerRank/HackerRank/TwoDArray.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank
+{
+    [TestFixture]
+    public class TwoDArray
+    {
+        [Test]
+        public void canFindMaxHourGlassSum()
+        {
+            Assert.AreEqual(0, hourglassSum(null));
+            Assert.AreEqual(1, hourglassSum(new int[][] {
+                new int[] { 1, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+            }), "1");
+            Assert.AreEqual(2, hourglassSum(new int[][] {
+                new int[] { 1, 1, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+            }), "2");
+            Assert.AreEqual(7, hourglassSum(new int[][] {
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+            }), "3");
+            Assert.AreEqual(7, hourglassSum(new int[][] {
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 1, 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+            }), "4");
+            Assert.AreEqual(9, hourglassSum(new int[][] {
+                new int[] { 1, 1, 1, 5, 0, 0 },
+                new int[] { 1, 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0 },
+            }), "5");
+            Assert.AreEqual(-6, hourglassSum(new int[][] {
+                new int[] { -1, -1, 0, -9, -2, -2 },
+                new int[] { -2, -1, -6, -8, -2, -5 },
+                new int[] { -1, -1, -1, -2, -3, -4 },
+                new int[] { -1, -9, -2, -4, -4, -5 },
+                new int[] { -7, -3, -3, -2, -9, -9 },
+                new int[] { -1, -3, -1, -2, -4, -5 },
+            }), "6");
+
+        }
+
+        private double hourglassSum(int[][] arr)
+        {
+            if (arr == null)
+                return 0;
+
+            int largestSum = -100;
+
+            for(int i = 0; i < arr.Length - 2; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < arr[i].Length - 2; j++)
+                {
+                    sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1] 
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                    if (sum > largestSum)
+                        largestSum = sum;
+                }
+            }
+
+
+            return largestSum;
+        }
+    }
+}
