531bdcfacacd1712105717a6607110ca90708f34
Dominic <dominicjosephenriquez@gmail.com>
2021-07-28 15:43:36 +0800

    Cracking Code Interview: Is Unique problem

---
+++ CarckingCodeInterview/CrackingCodeInterview/CrackingCodeInterview/IsUnique.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace CrackingCodeInterview
+{
+    [TestFixture]
+    public class IsUnique
+    {
+        [Test]
+        public void nothing()
+        {
+            Assert.That(isUniqueChars("abcd"), Is.True);
+            Assert.That(isUniqueChars("abcda"), Is.False);
+        }
+
+        bool isUniqueChars(string s)
+        {
+            if (s.Length > 128)
+                return false;
+
+            bool[] chars = new bool[128];
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                int c = s[i];
+                
+                if (chars[c])
+                    return false;
+
+                chars[c] = true;
+            }
+
+            return true;
+        }
+    }
+}
