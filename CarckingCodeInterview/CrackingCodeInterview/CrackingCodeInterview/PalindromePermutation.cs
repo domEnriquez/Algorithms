35c94ecb7c2cf1c52e157dc5e30bdfc378eb8403
Dominic <dominicjosephenriquez@gmail.com>
2021-07-30 18:53:06 +0800

    Own solution for palindrome permutation problem

---
+++ CarckingCodeInterview/CrackingCodeInterview/CrackingCodeInterview/PalindromePermutation.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CrackingCodeInterview
+{
+    [TestFixture]
+    public class PalindromePermutation
+    {
+        [Test]
+        public void IsPalindoromeTest()
+        {
+            Assert.That(isPalindrome("a"), Is.True);
+            Assert.That(isPalindrome("aba"), Is.True);
+            Assert.That(isPalindrome("aab"), Is.True);
+            Assert.That(isPalindrome("baa"), Is.True);
+            Assert.That(isPalindrome("aabb"), Is.True);
+            Assert.That(isPalindrome("baab"), Is.True);
+            Assert.That(isPalindrome("baabc"), Is.True);
+            Assert.That(isPalindrome("baadc"), Is.False);
+            Assert.That(isPalindrome("aabbb"), Is.True);
+            Assert.That(isPalindrome("tactcoa"), Is.True);
+            Assert.That(isPalindrome("aaaa"), Is.True);
+        }
+
+        public bool isPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            bool even = s.Length % 2 == 0 ? true : false;
+
+            Dictionary<char, int> charFreqDict = new Dictionary<char, int>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (charFreqDict.ContainsKey(s[i]))
+                    charFreqDict[s[i]] = ++charFreqDict[s[i]];
+                else
+                    charFreqDict[s[i]] = 1;
+            }
+
+            bool foundOdd = false;
+            foreach(KeyValuePair<char, int> el in charFreqDict)
+            {
+                if (el.Value % 2 != 0)
+                {
+                    if (foundOdd)
+                        return false;
+
+                    foundOdd = true;
+                }
+            }
+
+            return true;   
+        }
+        
+    }
+}
